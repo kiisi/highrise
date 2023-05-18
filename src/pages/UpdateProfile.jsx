120153d207c18c383555ba91d2d231e14bf86f3a
Kiisi <kiisifelix06@gmail.com>
2023-05-18 15:22:15 +0100

    Feat - Added Custom radio buttons

--- src/pages/UpdateProfile.jsx
+++ src/pages/UpdateProfile.jsx
@@ -22,18 +22,27 @@ const UpdateProfile = () => {
                                 <Input label="Other name" type="text" />
                             </fieldset>
                             <fieldset className="max-w-[400px]">
-                                <Input label="Address" type="text" />
-                            </fieldset>
-                            <fieldset className="max-w-[400px]">
                                 <Input label="Email" type="text" />
-                            </fieldset>
-                            <fieldset className="max-w-[400px]">
-                                <Input label="Age" type="number" />
                             </fieldset>
                             <fieldset className="max-w-[400px]">
                                 <Input label="Phone Number" type="number" />
                             </fieldset>
                         </div>
+                        <fieldset className="max-w-[400px] mt-10">
+                            <h1>Gender:</h1>
+                            <div className="flex gap-x-5 pt-2">
+                            <label htmlFor="hr-radio-male" className="flex gap-x-2 items-center cursor-pointer">
+                                <input type="radio" name="hr-radio" id="hr-radio-male" className="hr-radio-gender hidden"/>
+                                <span className="h-[26px] w-[26px] border-2 border-primary p-[4px] bg-[#fff] block rounded-[50%] hr-radio-btn"></span>
+                                <span>Male</span>
+                            </label>
+                            <label htmlFor="hr-radio-female" className="flex gap-x-2 items-center cursor-pointer">
+                                <input type="radio" name="hr-radio" id="hr-radio-female" className="hr-radio-gender hidden" />
+                                <span className="h-[26px] w-[26px] border-2 border-primary p-[4px] bg-[#fff] block rounded-[50%] hr-radio-btn"></span>
+                                <span>Female</span>
+                            </label>
+                            </div>
+                        </fieldset>
                         <Button className="mt-10">Update Profile</Button>
                     </div>
                 </section>
