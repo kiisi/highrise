498087a15254bc5d1b0dae5916a5c8a22a4bc76d
Kiisi <kiisifelix06@gmail.com>
2023-05-18 12:43:14 +0100

    Feat - Added Personal Informmation Page

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,8 +5,8 @@ const Input = ({ label, type }) => {
 
   return (
     <div className="relative pt-[10px] w-full">
-        <input type={type} className="border-[1px] border-[#dadce0] hover:border-primary focus:border-[transparent] caret-primary outline-none py-3 px-3 w-full rounded-[6px] focus:shadow-[0_0_0_2px_#684EA0]"/>
-        <label className="translate-y-[-10px] translate-x-[8px] bg-white px-[4px] absolute left-0 text-[14px] auth-label">{label}</label>
+        <input type={type} className="border-[1px] border-[#c0c1c4] hover:border-primary focus:border-[transparent] caret-primary outline-none py-3 px-3 w-full rounded-[6px] focus:shadow-[0_0_0_2px_#684EA0]"/>
+        <label className="translate-y-[-11px] translate-x-[8px] bg-white px-[4px] absolute left-0 text-[14px] auth-label">{label}</label>
     </div>
   )
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,15 +1,8 @@
 import { Link } from 'react-router-dom'
 import profile_pic from '../assets/profile-pics.svg'
+import Input from '../components/Input'
 
 
-
-// const  = [
-//     {
-//         to:'/',
-//         link:'Change of Names',
-
-//     }
-// ]
 const sidemenuLinks = [
     {
         icon: 'fa-solid fa-id-card',
@@ -34,7 +27,7 @@ const sidemenuLinks = [
 ]
 const Dashboard = () => {
     return (
-        <div className="min-h-[100vh] w-full flex">
+        <div className="min-h-[100vh] w-full flex bg-[#f8f8f8]">
             <aside className="w-full max-w-[260px] min-h-[100%] dashboard-sidenav flex flex-col pb-[20px]">
                 <div className="p-4 flex items-center gap-x-4 bg-[#214162]">
                     <figure>
@@ -73,13 +66,36 @@ const Dashboard = () => {
             <div className="flex-1">
                 <nav className="p-3 w-full bg-[#15283C] flex">
                     <ul className="flex gap-x-3 max-w-max ml-auto">
-                        <li href="#" className="text-white relative h-[25px] w-[25px]"><i className="fa fa-bell-o text-[20px] p-1"></i><span className="notification-badge">2</span></li>
+                        <li href="#" className="text-white relative h-[25px] w-[25px]"><i className="fa fa-bell-o text-[20px] p-1 c"></i><span className="notification-badge">2</span></li>
                         <li href="#" className="text-white relative h-[25px] w-[25px]"><i className="fa fa-question-circle text-[20px] p-1"></i></li>
                         <li href="#" className="text-white relative h-[25px] w-[25px]"><i className="fa fa-envelope-o text-[20px] p-1"></i><span className="notification-badge">2</span></li>
                     </ul>
                 </nav>
                 <main>
-
+                    <header className="pt-[25px] pr-[35px] pb-[22px] pl-[38px] shadow-[1px_0_5px_#0000001a]">
+                        <h1 className="text-primary text-[24px] font-bold">Personal Information</h1>
+                    </header>
+                    <section className="pt-[50px] pr-[35px] pb-[22px] pl-[38px]">
+                        <div className="bg-[#fff]">
+                            <div className="grid grid-cols-2 gap-x-5 gap-y-10">
+                                <fieldset className="max-w-[400px]">
+                                    <Input label="Full name" type="text"/>
+                                </fieldset>
+                                <fieldset className="max-w-[400px]">
+                                    <Input label="Address" type="text"/>
+                                </fieldset>
+                                <fieldset className="max-w-[400px]">
+                                    <Input label="Email" type="text"/>
+                                </fieldset>
+                                <fieldset className="max-w-[400px]">
+                                    <Input label="Age" type="number"/>
+                                </fieldset>
+                                <fieldset className="max-w-[400px]">
+                                    <Input label="Phone Number" type="number"/>
+                                </fieldset>
+                            </div>
+                        </div>
+                    </section>
                 </main>
             </div>
         </div>
