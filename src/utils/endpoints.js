2547d00c1c530a715beab6bf1c1d320b04e00046
Kiisi <kiisifelix06@gmail.com>
2023-05-25 13:19:11 +0100

    Changed base url endpoint

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -1,2 +1,2 @@
 // export const base_endpoint = "http://localhost:5000"
-export const base_endpoint = "https://maroon-crab-hat.cyclic.app"
+export const base_endpoint = "https://highrise-be.vercel.app"
