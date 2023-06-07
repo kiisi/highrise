bae1d82946e1875339364fb256a985590f39640f
Kiisi <kiisifelix06@gmail.com>
2023-06-07 08:56:21 +0100

    Feat - Changed endpoint to render

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -1,2 +1,3 @@
 // export const base_endpoint = "http://localhost:5000"
-export const base_endpoint = "https://maroon-crab-hat.cyclic.app"
+// export const base_endpoint = "https://maroon-crab-hat.cyclic.app"
+export const base_endpoint = "https://highrise-be.onrender.com"
